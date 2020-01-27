a9a0a02c4dd724c4f856da06ff9a4a37c689dc23
Pedro <pcordista368@gmail.com>
2020-01-27 17:08:50 -0300

    FrontEnd

---
+++ menu.php
@@ -0,0 +1,85 @@
+<section class="menu alignCenter desktop">
+    <div class="container">
+        <div class="menu-redes right-align">
+            <a href="#" target="_blank">
+                <i class="fab fa-facebook white-text"></i>
+            </a>
+            <a href="#" target="_blank">
+                <i class="fab fa-instagram white-text"></i>
+            </a>
+            <a href="#" target="_blank">
+                <i class="fab fa-linkedin white-text"></i>
+            </a>
+            <a href="#" target="_blank">
+                <i class="fab fa-youtube white-text"></i>
+            </a>
+        </div>
+        <div class="menu-row flex alignCenter">
+            <a href="index.php">
+                <img src="assets/img/sinclairpharma.png" alt="">
+            </a>
+            <div class="menu-links textUpper white-text flex alignCenter">
+                <a href="page-about.php">
+                    sobre
+                </a>
+                <a href="#">
+                    produtos
+                </a>
+                <a href="page-speakers.php">
+                    speakers
+                </a>
+                <a href="page-representantes.php">
+                    representantes
+                </a>
+                <a href="#">
+                    sinclair college
+                </a>
+                <a href="page-midias.php">
+                    na mídia
+                </a>
+                <a href="#">
+                    área do médico
+                </a>
+                <a href="#">
+                    área do representante
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="menumobile mobile">
+    <div class="menumobile_container relative">
+        <div class="menumobile_toogle valign-wrapper" id="sidebartoggle">
+            <i class="material-icons">menu</i>
+        </div>
+
+
+        <div class="menumobile_logo center">
+            <a href="index.php">
+                <img src="assets/img/sinclairpharma.png" alt="">
+            </a>
+        </div>
+    </div>
+
+</section>
+
+
+<div class="pushmenu pushmenu-right textCapitalize">
+    <div class="center">
+        <a href="index.php">
+            <img src="assets/img/sinclairpharma.png" alt="">
+        </a>
+    </div>
+    <a href="index.php">Home</a>
+    <a href="page-about.php">sobre</a>
+    <a href="#">produtos</a>
+    <a href="page-speakers.php">speakers</a>
+    <a href="page-representantes.php">representantes</a>
+    <a href="#">sinclair college</a>
+    <a href="page-midias.php">na mídia</a>
+    <a href="#">área do médico</a>
+    <a href="#">área do representante</a>
+
+</div>
